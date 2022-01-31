fc742d00aa3031dd4d2b1c06b37d4c02ea9c04dc
Pliszka <karoliniakkjk@gmail.com>
2022-01-31 23:26:03 +0100

    checking answer

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ data = pandas.read_csv('./us-states-game-start/50_states.csv')
 states = data.state.to_list()
 already_guessed = []
 guessed = len(already_guessed)
-answer = screen.textinput(title=f'Guessed {guessed}/50', prompt='What\'s another State\'s name?').capitalize()
-# if answer in states and answer not in already_guessed:
+game = True
+while game:
+    answer = screen.textinput(title=f'Guessed {guessed}/50', prompt='What\'s another State\'s name?').capitalize()
+    if answer in states and answer not in already_guessed:
 
+        already_guessed.append(answer)
+    if already_guessed = len(states):
+        game = False
 
 screen.mainloop()
