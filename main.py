4d8c52bc9c0e5d4fac89f8a16d41cf06fa08fbf5
Pliszka <karoliniakkjk@gmail.com>
2022-01-31 23:10:40 +0100

    input window

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title('U.S. States Game')
 image = './us-states-game-start/blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
 
+pandas.read_cvs('50')
 
-screen.exitonclick()
+
+guessed = 0
+answer = screen.textinput(title=f'Guessed {guessed}/50', prompt='What\'s another State\'s name?').capitalize()
+
+
+screen.mainloop()
