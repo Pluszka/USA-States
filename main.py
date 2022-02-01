63e63d25f0ae73561b4703e87af19af8f35447b6
Pliszka <karoliniakkjk@gmail.com>
2022-02-01 14:42:07 +0100

    Guessed states appear at map

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import turtle
 import pandas
 screen = turtle.Screen()
 screen.title('U.S. States Game')
-image = './us-states-game-start/blank_states_img.gif'
-screen.addshape(image)
-turtle.shape(image)
+IMAGE = './us-states-game-start/blank_states_img.gif'
+FONT = ['Arial', 10, 'normal']
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+Andy = turtle.Turtle()
+Andy.ht()
+Andy.penup()
+
 
 data = pandas.read_csv('./us-states-game-start/50_states.csv')
 states = data.state.to_list()
@@ -14,9 +19,13 @@ game = True
 while game:
     answer = screen.textinput(title=f'Guessed {guessed}/50', prompt='What\'s another State\'s name?').capitalize()
     if answer in states and answer not in already_guessed:
-
+        row = data[data.state == answer]
+        x_location = int(row.x)
+        y_location = int(row.y)
         already_guessed.append(answer)
-    if already_guessed = len(states):
+        Andy.goto(x_location, y_location)
+        Andy.write(answer, align='center', font=FONT)
+    if already_guessed == len(states):
         game = False
 
 screen.mainloop()
